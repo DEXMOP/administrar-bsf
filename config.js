cdeb985d490ddc356169f1b04d2adb9292ddede7
Elias <elias@example.com>
2026-06-08 07:30:14 -0500

    Completar configuracion por defecto con credenciales

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Configuración por defecto para BSF BioManager
 // Si completas estos campos, ningún dispositivo nuevo (PC o Celular) tendrá que configurarse manualmente.
 const DEFAULT_CONFIG = {
-    apiKey: "", // Pega aquí tu API Key de Google Cloud
-    clientId: "", // Pega aquí tu Google Client ID
-    spreadsheetId: "", // Pega aquí tu ID de la hoja de Google Sheets (Base de Datos)
-    driveFolderId: "" // Pega aquí el ID de la carpeta de Google Drive (donde se suben las fotos)
+    apiKey: "AIzaSyAK_S4n7KrPbkpBcgVATUk0QDS7XkD8m0c",
+    clientId: "553104051668-539qdqll4g3lr0jqpos4rm9qripi0s8e.apps.googleusercontent.com",
+    spreadsheetId: "1Zwqqj7ZyxPtNVo1fO3oOnWG5IC1hHZHS-vkx8K5poQ8",
+    driveFolderId: "1eMpwoIi1oM2pNPwv3AAAn3aTBIiASv-P"
 };
